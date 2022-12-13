263508b4d9ff70476fb9d0cc04856a90365c445c
Edwin Hati <edwinhati@outlook.com>
2022-12-13 19:38:26 +0800

    req-res

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -4,3 +4,17 @@ exports.hello = (req, res) => {
 exports.test = (req, res) => {
     res.send("Hello Test");
 }
+exports.test2 = (req, res) => {
+    const params = req.params.params;
+    res.json({
+        params
+    });
+};
+exports.calculate = (req, res) => {
+    const a = req.params.a;
+    const b = req.params.b;
+    const result = parseInt(a) + parseInt(b);
+    res.json({
+        result
+    });
+};
--- src/router/index.js
+++ src/router/index.js
@@ -4,5 +4,7 @@ const controller = require("../controllers/controller");
 
 router.get("/", controller.hello);
 router.get("/test", controller.test);
+router.get("/test2/:params", controller.test2);
+router.get("/calculate/:a/:b", controller.calculate);
 
 module.exports = router;
